688362efc1d2e85344411efa236e206f678fdf32
Prakhar <prakharb5645@gmail.com>
2024-11-05 16:44:24 +0530

    bug where export const is correct

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,7 +62,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.generateAccessToken = function (){
+userSchema.methods.generateAccessToken = function () {
     return jwt.sign(
         {
             _id: this._id,
@@ -77,7 +77,7 @@ userSchema.methods.generateAccessToken = function (){
     )
 }
 
-userSchema.methods.generateRefreshToken = function (){
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
             _id: this._id,
@@ -89,4 +89,4 @@ userSchema.methods.generateRefreshToken = function (){
     )
 }
 
-export default User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
