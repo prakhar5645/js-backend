3e3a642b77b6095203aacfd6a3095737617b84e0
Prakhar <prakharb5645@gmail.com>
2024-11-19 19:44:05 +0530

    video controller and some more fixes

--- src/app.js
+++ src/app.js
@@ -18,9 +18,13 @@ app.use(cookieParser())
 // routes
 
 import userRouter from "./routes/user.routes.js"
+import commentRouter from "./routes/comment.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);  //http://localhost:8000/api/v1/users/register
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/videos", videoRouter);
 
 
 // export default app;
