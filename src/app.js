1d03b6f0aa847bec385c944371e2dc073fdd3100
Prakhar <prakharb5645@gmail.com>
2024-11-25 17:10:54 +0530

    playlist controller and some fixes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import commentRouter from "./routes/comment.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 app.get("/", (req, res) => res.send("Backend of frontend"));
 
@@ -31,7 +32,7 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/likes", likeRouter);
-
+app.use("/api/v1/playlist", playlistRouter); 
 
 // export default app;
 export { app }; 
